ac20a9a50069cbf26b07fee8d9a78a5e45df946f
Praveen <deorani@gmail.com>
2018-08-05 16:39:14 +0800

    user registration

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import (LoginManager, login_user, current_user,
                             login_required, logout_user)
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-
+from flask_wtf import FlaskForm, CsrfProtect
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from werkzeug.security import generate_password_hash, check_password_hash
+   
 
 app = Flask(__name__)
 app.config.update(dict(
     SECRET_KEY="alanafalanadhimkana",
-    WTF_CSRF_SECRET_KEY="blahblah"
+    WTF_CSRF_SECRET_KEY="blahblah",
+    CSRF_SESSION_KEY="12345"
 ))
 
 
 login_manager = LoginManager()
 db = SQLAlchemy()
+csrf = CsrfProtect()
 
 
 class LoginForm(FlaskForm):
@@ -31,9 +35,10 @@ class User(db.Model):
     """
     __tablename__ = 'user'
 
-    email = db.Column(db.String, primary_key=True)
+    username = db.Column(db.String, primary_key=True)
     password = db.Column(db.String)
     authenticated = db.Column(db.Boolean, default=False)
+
 
     def is_active(self):
         """True, as all users are active."""
@@ -51,6 +56,35 @@ class User(db.Model):
         """False, as anonymous users aren't supported."""
         return False
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+# user = User()
+# user.username = 'deorani@example.com'
+# user.set_password('1234')
+# db.session.add(user)
+# db.session.commit()
 
 @login_manager.user_loader
 def user_loader(user_id):
@@ -60,25 +94,40 @@ def user_loader(user_id):
     return User.query.get(user_id)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    """For GET requests, display the login form.
-    For POSTS, login the current user by processing the form.
-
-    """
-    print(db)
-    form = LoginForm()
+    print('abc')
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm(request.form)
+    print(form.validate_on_submit())
+    print(form.errors)
     if form.validate_on_submit():
-        user = User.query.get(form.email.data)
+        user = User.query.filter_by(username=form.username.data).first()
         print(user)
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                user.authenticated = True
-                db.session.add(user)
-                db.session.commit()
-                login_user(user, remember=True)
-                return redirect(url_for("bull.reports"))
-    return render_template("login.html", form=form)
+        print(user.check_password(form.password.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
 
 @app.route("/logout", methods=["GET"])
 @login_required
@@ -100,6 +149,9 @@ def hello():
 
 if __name__ == "__main__":
 
+    user = User()
+
     login_manager.init_app(app)
     db.init_app(app)
+    csrf.init_app(app)
     app.run()
